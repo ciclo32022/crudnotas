c37d8e804f7b92b78ac05cf052785f9d9bd87bc9
crudnotas <mfjlgos@hotmail.com>
2023-09-30 08:41:12 -0500

    agregue modulo editar

--- editar.php
+++ editar.php
@@ -31,79 +31,79 @@ $persona = $sentencia->fetch(PDO::FETCH_OBJ);
     <link href="css/style.css" rel="stylesheet">
 </head>
 
-<body <!-- Section: Design Block -->
-    <section class="text-center">
-        <!-- Background image -->
-        <div class="p-5 bg-image" style="
+<body section class="text-center">
+    <!-- Background image -->
+    <div class="p-5 bg-image" style="
         background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg');
         height: 300px;
         "></div>
-        <!-- Background image -->
+    <!-- Background image -->
 
-        <div class="card mx-4 mx-md-5 shadow-5-strong" style="
+    <div class="card mx-4 mx-md-5 shadow-5-strong" style="
         margin-top: -100px;
         background: hsla(0, 0%, 100%, 0.8);
         backdrop-filter: blur(30px);
         ">
-            <div class="card-body py-5 px-md-5">
+        <div class="card-body py-5 px-md-5">
 
-                <div class="row d-flex justify-content-center">
-                    <div class="col-lg-8">
-                        <h2 class="fw-bold mb-5">EDITAR ALUMNO</h2>
-                        <form >
-                            <!-- 2 column grid layout with text inputs for the first and last names -->
-                            <div class="row justify-content-center">
-                                <div class="col-md-6 mb-4">
-
-
-
-
-                                    <div class="form-outline text-center">
-
-                                        <input class="form-control" id="periodo" type="text" name="txt2Paterno"
-                                            value="<?php echo $persona->ap_paterno; ?>" placeholder="Apellido Paterno"
-                                            autofocus required maxlength="15" size="6">
-                                            <br>
-                                        <input class="form-control" id="periodo" type="text" name="txtMaterno"
-                                            value="<?php echo $persona->ap_materno; ?>" placeholder="Apellido Materno"
-                                            autofocus required maxlength="15" size="6">
-                                            <br>
-                                        <input class="form-control" id="periodo" type="text" name="txtNombre"
-                                            placeholder="Nombre" value="<?php echo $persona->nombre; ?>" autofocus
-                                            required maxlength="15" size="6">
-                                            <br>
-                                        <input class="form-control" id="periodo" type="text" name="txtParcial"
-                                            value="<?php echo $persona->ex_parcial; ?>" placeholder="Nota Parcial"
-                                            autofocus required maxlength="15" size="6">
-                                            <br>
-                                        <input class="form-control" id="periodo" type="text" name="txtFinal"
-                                            placeholder="Nota Final" value="<?php echo $persona->ex_final; ?>" autofocus
-                                            required maxlength="15" size="6">
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="fw-bold mb-5">EDITAR ALUMNO</h2>
+                    <form method="POST" action="editarProceso.php">
+                        <!-- 2 column grid layout with text inputs for the first and last names -->
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 mb-4">
 
 
 
-                                    </div>
+
+                                <div class="form-outline text-center">
+
+                                    <input class="form-control" id="periodo" type="text" name="txt2Paterno"
+                                        value="<?php echo $persona->ap_paterno; ?>" placeholder="Apellido Paterno"
+                                        autofocus required maxlength="15" size="6">
+                                    <br>
+                                    <input class="form-control" id="periodo" type="text" name="txt2Materno"
+                                        value="<?php echo $persona->ap_materno; ?>" placeholder="Apellido Materno"
+                                        autofocus required maxlength="15" size="6">
+                                    <br>
+                                    <input class="form-control" id="periodo" type="text" name="txt2Nombre"
+                                        placeholder="Nombre" value="<?php echo $persona->nombre; ?>" autofocus required
+                                        maxlength="15" size="6">
+                                    <br>
+                                    <input class="form-control" id="periodo" type="text" name="txt2Parcial"
+                                        value="<?php echo $persona->ex_parcial; ?>" placeholder="Nota Parcial" autofocus
+                                        required maxlength="15" size="6">
+                                    <br>
+                                    <input class="form-control" id="periodo" type="text" name="tx2tFinal"
+                                        placeholder="Nota Final" value="<?php echo $persona->ex_final; ?>" autofocus
+                                        required maxlength="15" size="6">
+
+
 
                                 </div>
 
                             </div>
 
+                        </div>
 
-                    </div>
 
-                    </label>
                 </div>
 
-                <!-- Submit button -->
-                <<input type="text" name="oculto" value="">
-                <button type="submit" class="btn btn-primary btn-block mb-4">
-                    EDITAR ALUMNO
-                </button>
-                </form>
+                </label>
             </div>
+
+            <!-- Submit button -->
+
+
+            <input type="hidden" name="oculto">
+            <button type="submit" class="btn btn-primary btn-block mb-4"> EDITAR ALUMNO </button>
+            </form>
+           
         </div>
-        </div>
-        </div>
+    </div>
+    </div>
+    </div>
     </section>
     <!-- Section: Design Block -->
 
